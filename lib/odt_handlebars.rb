7bb32f9b0944d1a711941db97be218701a2af4b0
Peter Schrammel <peter.schrammel@gmx.de>
2020-03-29 22:52:38 +0200

    adding Changelog

---
+++ lib/odt_handlebars.rb
@@ -0,0 +1,6 @@
+require "odt_handlebars/version"
+
+module OdtHandlebars
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/odt_handlebars/version.rb
@@ -0,0 +1,3 @@
+module OdtHandlebars
+  VERSION = "0.1.0"
+end
